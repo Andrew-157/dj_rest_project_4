33f5d7cf0cbe81b58b0fed53933762c21bbf5d60
Andrew Subotin <subotinandrey5@gmail.com>
2023-09-16 11:07:38 +0200

    UserReadSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -6,3 +6,9 @@ class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
+
+
+class UserReadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
