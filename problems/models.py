daf866525d728bd3106e0a15ffe501a2dc9da962
Andrew Subotin <subotinandrey5@gmail.com>
2023-09-16 10:48:08 +0200

    New models: Problem, Tag

--- problems/models.py
+++ problems/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=300)
+
+    def save(self, *args, **kwargs) -> None:
+        self.name = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Problem(models.Model):
+    title = models.CharField(max_length=255, validators=[
+                             MinLengthValidator(limit_value=5,
+                                                message="The problem's title cannot be shorter than 5 characters.")])
+    body = models.TextField(validators=[MinLengthValidator(limit_value=10,
+                                                           message="The problem's body cannot be shorter than 10 characters.")])
+    user = models.ForeignKey(
+        'auth.User', on_delete=models.CASCADE, related_name='problems')
+    published = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return self.title
--- solvee/settings.py
+++ solvee/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'rest_framework',
     'oauth2_provider',
+    'problems',
 ]
 
 MIDDLEWARE = [
